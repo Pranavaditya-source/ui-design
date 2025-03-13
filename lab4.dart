0ed42a91a70bec014032321b6d3eeab3aaa2c895
Pranavaditya-source <pranavadityabuddhavarapu@gmail.com>
2025-03-13 12:22:02 +0530

    Add files via upload

---
+++ lab4.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+	void fruit(){
+		var fruit1='apple';
+		switch(fruit1){
+			case 'Apple':{print("apple is available");}
+			break;
+			
+			case 'APPLE':{print("apple is available");}
+			break;
+
+			case 'APPle':{print("apple is available");}
+			break;
+
+			case 'appLE':{print("apple is available");}
+			break;
+
+			default:{print("either your input is incorrect or apple is not available");}
+			break;
+		}
+		 
+}
